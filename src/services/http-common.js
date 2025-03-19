a01779b30d1864da43f280b73ef9f32fdbe0df18
jakarwan <jakarwan_159@hotmail.com>
2025-03-19 23:19:32 +0700

    fix

---
+++ src/services/http-common.js
@@ -0,0 +1,9 @@
+import Axios from "axios";
+
+// const API_ENDPOINT = "http://localhost:3035/";
+// const API_ENDPOINT = "https://lottoapi.pg-th.com/";
+const API_ENDPOINT = "https://apieagle168.laosspecialvip.com/";
+
+export const HTTP = Axios.create({
+  baseURL: API_ENDPOINT,
+});
